e2ed44afcb380a568fa0f55e3b3287dc9c20f549
Shashank Pathipati <pshashank569@gmail.com>
2023-09-04 14:21:06 +0530

    - added route files - installed primevue

---
+++ plugins/primevue.ts
@@ -0,0 +1,7 @@
+import PrimeVue from "primevue/config";
+import ToastService from "primevue/toastservice";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(PrimeVue);
+  nuxtApp.vueApp.use(ToastService);
+});
