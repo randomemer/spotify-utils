eee0eb83cbea3d9e782fba6b75e3892c6aa113c4
Shashank Pathipati <pshashank569@gmail.com>
2023-09-08 20:38:11 +0530

    fixed vercel env var

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   origin: process.dev
     ? "http://localhost:4009"
-    : `https://${process.env.VERCEL_DOMAIN}`,
+    : `https://${process.env.VERCEL_URL}`,
   scopes: [
     // user
     "user-read-email",
